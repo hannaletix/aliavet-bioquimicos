f6c109f2fbad9bb68739f1ed9ce7e535ed177438
Hanna Letícia <hanna_45leticia@hotmail.com>
2024-03-04 22:38:21 -0300

    feat: addition of loading, success modal and mobile stylization

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,53 @@
 document.getElementById('submitBtn').addEventListener('click', function() {
+    document.getElementById('loading').style.display = 'block';
+
     var formData = new FormData();
-    formData.append('fileUpload', document.getElementById('fileUpload').files[0]);
+    formData.append('fileUpload', document.getElementById('hiddenFileUpload').files[0]);
 
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '/upload', true);
 
     xhr.onload = function () {
+        document.getElementById('loading').style.display = 'none';
+
         if (xhr.status === 200) {
-            // Sucesso
-            document.getElementById('message').innerText = 'Arquivo enviado com sucesso!';
+            document.getElementById('modal').style.display = 'block';
         } else {
-            // Erro
-            document.getElementById('message').innerText = 'Falha no envio do arquivo.';
+            document.getElementById('modal').style.display = 'none';
         }
+    };
+
+    xhr.onerror = function () {
+        document.getElementById('loading').style.display = 'none';
+        document.getElementById('modal').style.display = 'none';
     };
 
     xhr.send(formData);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const customBtn = document.getElementById('fileUpload');
+    const realFileBtn = document.getElementById('hiddenFileUpload');
+    const customTxt = document.getElementById('fileChosen');
+
+    if (customBtn) {
+        customBtn.addEventListener('click', function() {
+            realFileBtn.click();
+        });
+    }
+
+    if (realFileBtn) {
+        realFileBtn.addEventListener('change', function() {
+            if (realFileBtn.value) {
+                customTxt.innerHTML = realFileBtn.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1]; // Extrai apenas o nome do arquivo
+            } else {
+                customTxt.innerHTML = 'Nenhum arquivo escolhido';
+            }
+        });
+    }
+});
+
+document.querySelector('.close-button').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'none';
+});
+
